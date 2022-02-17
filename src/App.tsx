c6d0d1407b9c8ed10278aa69a41b99e3e532409d
Luis Fernando Cezar dos Santos <nandocezar22@gmail.com>
2022-02-17 17:23:27 -0300

    init project

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Page from './components/Page';
+import GlobalStyles from './styles/GlobalStyles';
+
+function App() {
+  return (
+    <>
+    <Page />
+    <GlobalStyles />
+    </>
+    
+  );
+}
+
+export default App;
